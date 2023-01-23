a4f9cf429bd184dd0d7bff1dfc258ec9696ab834
angie <angie.v.m.rojas@gmail.com>
2023-01-23 17:52:12 +0100

    version 1.03

--- conexion.php
+++ conexion.php
@@ -1,9 +1,11 @@
 
 	//CAMBIAR LO DE ABAJO CON INDEX.HTM ==================================
 	<?php
-	$conexion = mysqli_connect('localhost','root', '','Empresas') 
-	or die(mysqli_error($mysqli))
-	insertar($conexion);
+	require_once 'pdoconfig.php';
+	try {
+		$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+		echo "Connected to $dbname at $host successfully.";
+		insertar($conexion);
 
 	function insertar ($conexion) {
 		
@@ -14,17 +16,22 @@
 		$desplegable_sala = $_POST['salas'];
 		$duracion_evento = $_POST['duracion_evento'];
 		$patrocinador = $_POST['patrocinador'];
-		$artista = $_POST['artista']
-		$trabajadores = $_POST['trabajadores']
+		$artista = $_POST['artista'];
+		$trabajadores = $_POST['trabajadores'];
 		$aceptar_rpg = $_POST['aceptar_rpg'];
 		$asunto = 'Formulario Rellenado';
 
 
-		$consulta = "INSERT INTO Empresas () VALUES ()"
+		$consulta = "INSERT INTO Empresas () VALUES ($nombre,$apellidos,$numero,$email
+		,$desplegable_sala, $duracion_evento, $patrocinador, $artista, $trabajadores, $aceptar_rpg)"
+	
+	} catch (PDOException $pe) {
+		die("Could not connect to the database $dbname :" . $pe->getMessage());
 	}
 
 	
 	
+
+	
+	
  ?>
-  </body>
-</html>
